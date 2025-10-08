8802f9d00392e0a47b1587ddddbb442f19f8be7c
Indhumathi Maheswaran <indhu_7@yahoo.com>
2025-10-08 14:33:02 +0800

    Add files via upload

--- Univariate.py
+++ Univariate.py
@@ -1,5 +1,6 @@
-class univariate():
-#QuanQual Function
+import pandas as pd
+import numpy as np
+class univariate:
     def QuanQual(dataset):
         qual = []
         quan = []
@@ -9,3 +10,45 @@ class univariate():
             else:
                 quan.append(ColumnName)
         return quan, qual
+    def Centre_Percentile_IQR(quan,dataset):
+            descriptive = pd.DataFrame(index = ["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%","Q4:100%","IQR","1.5 Rule","Lesser","Greater","Min","Max"],columns = quan)
+            #Mean
+            for ColumnName in quan:
+                descriptive[ColumnName]["Mean"] = dataset[ColumnName].mean()
+            #Median
+                descriptive[ColumnName]["Median"] = dataset[ColumnName].median()
+            #Mode
+                descriptive[ColumnName]["Mode"] = dataset[ColumnName].mode()[0]
+            #Percentile
+                descriptive[ColumnName]["Q1:25%"] = dataset.describe()[ColumnName]["25%"]
+                descriptive[ColumnName]["Q2:50%"] = dataset.describe()[ColumnName]["50%"]
+                descriptive[ColumnName]["Q3:75%"] = dataset.describe()[ColumnName]["75%"]
+                descriptive[ColumnName]["99%"] = np.percentile(dataset[ColumnName],99)
+                descriptive[ColumnName]["Q4:100%"] = dataset.describe()[ColumnName]["max"]
+            #IQR
+                descriptive[ColumnName]["IQR"] = descriptive[ColumnName]["Q3:75%"] - descriptive[ColumnName]["Q1:25%"]
+                descriptive[ColumnName]["1.5 Rule"] = 1.5 * descriptive[ColumnName]["IQR"]
+                descriptive[ColumnName]["Lesser"] = descriptive[ColumnName]["Q1:25%"] -  descriptive[ColumnName]["1.5 Rule"]
+                descriptive[ColumnName]["Greater"] = descriptive[ColumnName]["Q3:75%"] +  descriptive[ColumnName]["1.5 Rule"]
+                descriptive[ColumnName]["Min"] = dataset[ColumnName].min()
+                descriptive[ColumnName]["Max"] = dataset[ColumnName].max()
+            return descriptive
+    #Function for Lesser and Greater Outliers
+    def less_great_outlier(quan,descriptive):
+        lesser = []
+        greater = []
+    
+        for ColumnName in quan:
+            if(descriptive[ColumnName]["Min"]< descriptive[ColumnName]["Lesser"]):
+                lesser.append(ColumnName)
+            if(descriptive[ColumnName]["Max"]> descriptive[ColumnName]["Greater"]):
+                greater.append(ColumnName)
+        return lesser, greater
+    #Replacing Outliers
+    def Replace_Outlier(dataset, lesser, greater, descriptive):
+        for ColumnName in lesser: 
+            dataset[ColumnName][dataset[ColumnName]< descriptive[ColumnName]["Lesser"]] = descriptive[ColumnName]["Lesser"]
+        for ColumnName in greater: 
+            dataset[ColumnName][dataset[ColumnName]> descriptive[ColumnName]["Greater"]] = descriptive[ColumnName]["Greater"] 
+        return dataset
+    
